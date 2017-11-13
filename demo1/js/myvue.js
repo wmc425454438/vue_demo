e3015561f363bc949909486a0263c29f40ac4407
wmc425454438 <wmc2012wxm@163.com>
2017-11-14 01:11:05 +0800

    demo1

---
+++ demo1/js/myvue.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Michael on 2017/11/13.
+ */
+new Vue({
+    el: '#dropdown',
+    data: {
+        sites: [
+            { name: 'Action', url: '#'},
+            { name: 'another action', url: '#'},
+            { name: 'something action else', url: '#'}
+        ]
+
+    }
+});
+
+new Vue({
+    el: '#content',
+    data: {
+        cons: [
+            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'},
+            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'},
+            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'}
+        ]
+
+    }
+});
