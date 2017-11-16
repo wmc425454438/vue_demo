a141e1fea23e36df296cc523107ee17add5af7a0
wmc425454438 <wmc2012wxm@163.com>
2017-11-16 23:52:43 +0800

    bootstrap+vue 测试

--- demo1/js/myvue.js
+++ demo1/js/myvue.js
@@ -2,12 +2,12 @@
  * Created by Michael on 2017/11/13.
  */
 new Vue({
-    el: '#dropdown',
+    el: '#navbar',
     data: {
         sites: [
-            { name: 'Action', url: '#'},
-            { name: 'another action', url: '#'},
-            { name: 'something action else', url: '#'}
+            { name: 'Action', url: '#', active:'active'},
+            { name: 'another action', url: '#',active:''},
+            { name: 'something action else', url: '#',active:''}
         ]
 
     }
@@ -17,10 +17,16 @@ new Vue({
     el: '#content',
     data: {
         cons: [
-            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'},
-            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'},
-            { title: 'Heading', content: 'hahahahahhaahahahahahahahahahah', url: '#'}
-        ]
+            { id:'c1', title: 'Heading', class: 'active',content: 'entum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+            { id:'c2',title: 'Heading', content: 'ntum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+            { id:'c3',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+        { id:'c4',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		{ id:'c5',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		{ id:'c6',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		{ id:'c7',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		{ id:'c8',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		{ id:'c9',title: 'Heading', content: ' nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.tiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ', url: '#'},
+		]
 
     }
 });
